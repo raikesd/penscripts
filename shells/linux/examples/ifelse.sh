d55495b2024c767df3bf31c6eb5b8539f4f931f2
Donald Raikes <don.raikes@gmail.com>
2014-01-12 03:54:32 -0700

    adding example scripts

---
+++ shells/linux/examples/ifelse.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$1" = "Neo." ]; then
+MESSAGE="Wake up, "$1
+else
+MESSAGE="Hey, you're not Neo"
+fi
+echo $MESSAGE
---
+++ shells/linux/examples/vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+function localmessage
+{
+local MESSAGE="Hi there, we're inside the function"
+echo $MESSAGE
+}
+MESSAGE="Hello world, we're outside the function"
+echo $MESSAGE
+localmessage
+echo $MESSAGE
